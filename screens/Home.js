b61acfd16c597c0b685a0a422bc1d2fa8842c5b4
Beis Presheva <beis@Beiss-MacBook-Pro.local>
2022-06-24 00:19:10 +0200

    Bottom Tabs Commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categories from '../components/Categories';
-import HeaderTabs from '../components/HeaderTabs'
+import HeaderTabs from '../components/HeaderTabs';
+import BottomTabs from '../components/BottomTabs';
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 import SearchBar from '../components/SearchBar';
 import RestaurantItemsHorizontal from '../components/RestaurantItemsHorizontal';
+import { Divider } from 'react-native-elements/dist/divider/Divider';
 
 const YELP_API_KEY = "gwggNHvf81V0FiS2J0YZfqopGfAj4PEkk4INOZQ5WAVwX9UAcqnIzfpHfTnseJ70juod7Nz-9Z-gFjceAbguxByDXAVdbyeVlebq8onLNtTAGOPDsS3ykEySVkuvYnYx";
 
@@ -64,6 +66,8 @@ export default function Home() {
                 <RestaurantItemsHorizontal newestRestaurantData={newestRestaurantData}/>
                 <RestaurantItems restaurantData={restaurantData}/>
             </ScrollView>
+            <Divider width={1} />
+            <BottomTabs />
         </SafeAreaView>
     );
 }
