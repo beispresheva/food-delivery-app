97cc0c360e572338097768b71930da6f190396d1
Beis Presheva <beis@Beiss-MacBook-Pro.local>
2022-06-26 23:12:49 +0200

    Restaurant Detail Info Dynamic Passing Commit

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
 
-const image = "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/d/p29410-15743151555dd62493bdda1.jpg?tr=tr:n-xlarge";
+const yelpRestaurantInfo = {
+    name: 'Farmhouse Kitchen Thai Cuisine',
+    image: "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/d/p29410-15743151555dd62493bdda1.jpg?tr=tr:n-xlarge",
+    display_phone: "+14157492060",
+    location: {
+        address1: "800 N Point St",
+        city: "San Francisco"
+    }
+};
 
-const title = 'Test Resturant Cuisine';
-const description = 'Lorem ipsum dolor amet sinid amet dolor isit';
+const {name, image, display_phone, location} = yelpRestaurantInfo;
+
+const LocationAddress = location.address1;
+const LocationCity = location.city;
+
+const description = `${LocationAddress} • ${LocationCity} • ${display_phone}`;
 
 export default function About() {
     return (
         <View style={{padding: 20}}>
             <RestaurantImage image={image} />
             
-            <RestaurantTitle title={title}/>
+            <RestaurantName name={name}/>
 
             <RestaurantDescription description={description} />
         </View>
@@ -22,10 +34,10 @@ const RestaurantImage = (props) => (
     <Image source={{uri: props.image}} style={{width: "100%", height: 140, borderRadius: 8}} />
 );
 
-const RestaurantTitle = (props) => (
-    <Text style={{fontSize: 25, fontWeight: "600", marginTop: 30}}>{props.title}</Text>
+const RestaurantName = (props) => (
+    <Text style={{fontSize: 25, fontWeight: "600", marginTop: 30}}>{props.name}</Text>
 );
 
 const RestaurantDescription = (props) => (
-    <Text style={{fontSize: 16, color: 'gray', marginTop: 6}}>{props.description}</Text>
+    <Text style={{fontSize: 14, color: 'gray', marginTop: 6}}>{props.description}</Text>
 );
